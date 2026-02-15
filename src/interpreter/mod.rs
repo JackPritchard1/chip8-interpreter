80bb770ce4c017149911aefff59f20c089fa74ef
jackpritchard1 <pritchardj1@icloud.com>
2026-02-15 12:07:50 +0000

    added opcodes

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -54,7 +54,50 @@ impl Interpreter for State {
     }
 }
 
-fn execute(opcode: u8) {
-
+fn execute(opcode: (u8, u8)) {
+    let nib = (opcode.0 & 0b1111_0000, opcode.0 & 0b0000_1111, opcode.1 & 0b1111_0000, opcode.1 & 0b0000_1111);
+    let x = nib.1;
+    let y = nib.2;
+    let n = nib.3;
+    let kk = nib.2 << 4 | nib.3;
+    let nnn = ((nib.1 as u16) << 8 | (nib.2 as u16) << 4 | (nib.3 as u16));
+    match nib {
+        (0, 0, 0, 0) => nop(),
+        (0, 0, 0xE, 0x0) => clear(),
+        (0, 0, 0xE, 0xE) => ret(),
+        (0, _, _, _) => sys(nnn),
+        (1, _, _, _) => jp(nnn),
+        (2, _, _, _) => call(nnn),
+        (3, _, _, _) => se(x, kk),
+        (4, _, _, _) => sne(x, kk),
+        (5, _, _, 0) => se(x, y),
+        (6, _, _, _) => ld(x, kk),
+        (7, _, _, _) => add(x, kk),
+        (8, _, _, 0) => ld(x, y),
+        (8, _, _, 1) => or(x, y),
+        (8, _, _, 2) => and(x, y),
+        (8, _, _, 3) => xor(x, y),
+        (8, _, _, 4) => add(x, y),
+        (8, _, _, 5) => sub(x, y),
+        (8, _, _, 6) => shr(x, y),
+        (8, _, _, 7) => subn(x, y),
+        (8, _, _, 0xE) => shl(x, y),
+        (9, _, _, 0) => sne_reg(x, y),
+        (0xA, _, _, _) => ld(nnn),
+        (0xB, _, _, _) => jp_v0(nnn),
+        (0xC, _, _, _) => rnd(x, kk),
+        (0xD, _, _, _) => drw(x, y, n),
+        (0xE, _, 9, 0xE) => skp(x),
+        (0xE, _, 0xA, 1) => sknp(x),
+        (0xF, _, 0, 7) => ld_Vx_DT(x),
+        (0xF, _, 0, 0xA) => ld_K(x),
+        (0xF, _, 1, 5) => ld_DT_Vx(x),
+        (0xF, _, 1, 8) => ld_ST_Vx(x),
+        (0xF, _, 1, 0xE) => add_I(x),
+        (0xF, _, 2, 9) => ld_F(x),
+        (0xF, _, 3, 3) => ld_B(x),
+        (0xF, _, 5, 5) => ld_I_Vx(x),
+        (0xF, _, 6, 5) => ld_Vx_I(x),
+    }
 }
 
